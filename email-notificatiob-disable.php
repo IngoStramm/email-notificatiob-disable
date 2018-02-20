1e119b154fbe9e0e9f16ad4b2472e8b82826f4c6
Ingo Stramm <ingostramm@gmail.com>
2018-02-20 14:43:49 -0300

    Initial commit

---
+++ email-notificatiob-disable.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Plugin Name: E-mail Notification Disable
+Plugin URI: http://laf.marketing
+Description: Este plugin desabilita o envio de e-mail para o administrador quando a senha de um usuário é alterada.
+Version: 1.0
+Author: Ingo Stramm
+License: GPLv2
+*/
+
+add_action( 'init', 'email_notification_disable' );
+
+function email_notification_disable() {
+
+	add_filter( 'send_email_change_email', '__return_false' );
+
+} 
